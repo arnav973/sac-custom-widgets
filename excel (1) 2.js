8d551ae9d7bee6467b864bc1280a9674c5236ea6
Arnav Choudhary <arnavchoudhary97373@gmail.com>
2026-01-20 11:41:35 +0530

    Update excel (1) 2.js

--- excel (1) 2.js
+++ excel (1) 2.js
@@ -135,7 +135,7 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             var that = this;
 
-            let xlsxjs = "https://priyankayala.github.io/ExcelUpload/xlsx.js";
+            let xlsxjs = "https://ntt.github.io/ExcelUpload/xlsx.js";
             async function LoadLibs() {
                 try {
                     await loadScript(xlsxjs, _shadowRoot);
