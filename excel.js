beeb4de4e2c1c0864588fb41e42fca7970ab92f9
Arnav Choudhary <arnavchoudhary97373@gmail.com>
2026-01-20 12:40:13 +0530

    Update excel.js

--- excel.js
+++ excel.js
@@ -135,7 +135,7 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             var that = this;
 
-            let xlsxjs = "https://ntt.github.io/sac-custom-widgets/xlsx.js";
+            let xlsxjs = "https://arnav973/sac-custom-widgets/xlsx.js";
             async function LoadLibs() {
                 try {
                     await loadScript(xlsxjs, _shadowRoot);
